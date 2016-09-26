b8432c1fcb70260b9c87d10315d2f7fc059c3613
Akihiko Oikawa <oikawa@chiba-fjb.ac.jp>
2016-09-26 19:17:26 +0900

    カメラ画像をGoogleDriveへ出力

---
+++ app/src/main/java/jp/ac/chiba_fjb/x14b_b/daichan/CameraFragment.java
@@ -0,0 +1,115 @@
+package jp.ac.chiba_fjb.x14b_b.daichan;
+
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.TextureView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CameraFragment extends Fragment implements View.OnTouchListener, CameraPreview.SaveListener {
+
+
+    private CameraPreview mCamera;
+    private GoogleDrive mDrive;
+
+    public CameraFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_camera, container, false);;
+        view.setOnTouchListener(this);
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+
+        mCamera = new CameraPreview();
+        TextureView textureView = (TextureView)getView().findViewById(R.id.textureView);
+        mCamera.setTextureView(textureView);
+        mCamera.open(0);
+        mCamera.startPreview();
+        mCamera.setSaveListener(this);
+    }
+
+    @Override
+    public void onPause() {
+
+        mCamera.close();
+        //mCamera.
+        super.onPause();
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        mCamera.save();
+        return false;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mDrive = new GoogleDrive(getActivity());
+        mDrive.connect();
+
+    }
+
+    @Override
+    public void onStop() {
+        mDrive.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onSave(final Bitmap bitmap) {
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
+                final String filename = sdf.format(new Date())+".jpeg";
+
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Snackbar.make(getView(), filename+"を保存中", Snackbar.LENGTH_SHORT).show();
+                    }
+                });
+
+
+                GoogleDrive.Folder f = mDrive.getFolder().createFolder("CamData");
+                f.uploadBitmap(filename,bitmap);
+                System.out.println("出力");
+
+
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Snackbar.make(getView(), filename+"を保存完了", Snackbar.LENGTH_SHORT).show();
+                    }
+                });
+
+
+            }
+        }.start();
+    }
+}
---
+++ app/src/main/java/jp/ac/chiba_fjb/x14b_b/daichan/GoogleDrive.java
@@ -0,0 +1,138 @@
+package jp.ac.chiba_fjb.x14b_b.daichan;
+
+import android.app.Activity;
+import android.app.DownloadManager;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentActivity;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.drive.Drive;
+import com.google.android.gms.drive.DriveApi;
+import com.google.android.gms.drive.DriveContents;
+import com.google.android.gms.drive.DriveFolder;
+import com.google.android.gms.drive.Metadata;
+import com.google.android.gms.drive.MetadataChangeSet;
+import com.google.android.gms.drive.query.Filters;
+import com.google.android.gms.drive.query.Query;
+import com.google.android.gms.drive.query.SearchableField;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * Created by oikawa on 2016/09/26.
+ */
+
+public class GoogleDrive implements  GoogleApiClient.OnConnectionFailedListener, ResultCallback<DriveApi.DriveContentsResult> {
+    public class Folder{
+        private DriveFolder mFolder;
+
+        public Folder(){
+            mFolder = Drive.DriveApi.getRootFolder(mGoogleApiClient);
+        }
+        public Folder(DriveFolder folder){
+            mFolder = folder;
+        }
+        public void uploadBitmap(String fileName,Bitmap bitmap){
+            MetadataChangeSet changeSet = new MetadataChangeSet.Builder()
+                    .setTitle(fileName)
+                    .setMimeType("image/jpg")
+                    .setStarred(false).build();
+
+            DriveApi.DriveContentsResult result = Drive.DriveApi.newDriveContents(mGoogleApiClient).await();
+            DriveContents contents = result.getDriveContents();
+            try {
+                OutputStream fos = contents.getOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+                // 保存処理終了
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            mFolder.createFile(mGoogleApiClient, changeSet, contents).await();
+
+        }
+        public Folder createFolder(String name){
+            //既存のフォルダを検索
+            Folder f = getFolder(name);
+            if(f != null)
+                return f;
+            //新規作成
+            MetadataChangeSet changeSet = new MetadataChangeSet.Builder().setTitle(name).build();
+            PendingResult<DriveFolder.DriveFolderResult> a = mFolder.createFolder(mGoogleApiClient, changeSet);
+            DriveFolder.DriveFolderResult result = a.await();
+            if(!result.getStatus().isSuccess())
+                return null;
+            return new Folder(result.getDriveFolder());
+        }
+        public Folder getFolder(String name){
+            Query q = new Query.Builder().addFilter(Filters.eq(SearchableField.MIME_TYPE, "application/vnd.google-apps.folder")).addFilter(Filters.eq(SearchableField.TITLE, name)).build();
+            PendingResult<DriveApi.MetadataBufferResult> result = mFolder.queryChildren(mGoogleApiClient,q);
+            DriveApi.MetadataBufferResult values = result.await();
+
+            if(values.getMetadataBuffer().getCount() == 0)
+                return null;
+            return new Folder(values.getMetadataBuffer().get(0).getDriveId().asDriveFolder());
+        }
+        public void query(){
+
+            PendingResult<DriveApi.MetadataBufferResult> result = mFolder.listChildren(mGoogleApiClient);
+            DriveApi.MetadataBufferResult values = result.await();
+
+            int count = values.getMetadataBuffer().getCount();
+            for(int i = 0; i<count ;i++){
+                Metadata m = values.getMetadataBuffer().get(i);
+                System.out.println(m.getMimeType()+(m.getOriginalFilename() != null?m.getOriginalFilename() :m.getTitle()));
+            }
+            values.release();
+        }
+
+    }
+
+
+    private GoogleApiClient mGoogleApiClient;
+
+    public GoogleDrive(FragmentActivity con){
+        mGoogleApiClient = new GoogleApiClient.Builder(con)
+                .addApi(Drive.API)
+                .addScope(Drive.SCOPE_FILE)
+                .enableAutoManage(con, this)
+                .build();
+    }
+
+    boolean connect(){
+         mGoogleApiClient.connect();
+        //DriveApi.DriveContentsResult result = Drive.DriveApi.newDriveContents(mGoogleApiClient).await();
+        //if(!result.getStatus().isSuccess())
+        //    return false;
+
+        return true;
+    }
+    public void disconnect() {
+        if(mGoogleApiClient != null)
+            mGoogleApiClient.disconnect();
+    }
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onResult(@NonNull DriveApi.DriveContentsResult driveContentsResult) {
+        if(driveContentsResult.getStatus().isSuccess())
+            Drive.DriveApi.requestSync(mGoogleApiClient).await();
+    }
+
+    Folder getFolder(){
+        return new Folder();
+    }
+
+
+}
